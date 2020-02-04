9e90e41b465b98a244f73bc0645869a1544bcad8
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2020-02-04 12:41:53 -0500

    Initial Commit

---
+++ i2020_02_04Tests/i2020_02_04Tests.swift
@@ -0,0 +1,34 @@
+//
+//  i2020_02_04Tests.swift
+//  i2020_02_04Tests
+//
+//  Created by Bradford, Phillip on 2/4/20.
+//  Copyright © 2020 Bradford, Phillip. All rights reserved.
+//
+
+import XCTest
+@testable import i2020_02_04
+
+class i2020_02_04Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
